df88a4ccf4058186315f3b20444e88f600c843cd
Anthony Powles <github@idreamz.net>
2014-01-21 09:24:02 -0800

    added custom cells, WIP

---
+++ ToDoList/IDZToDoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDZToDoListViewController.h
+//  ToDoList
+//
+//  Created by Anthony Powles on 20/01/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDZToDoListViewController : UITableViewController
+
+@end
---
+++ ToDoList/IDZViewCell.m
@@ -0,0 +1,33 @@
+//
+//  IDZViewCell.m
+//  ToDoList
+//
+//  Created by Anthony Powles on 20/01/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import "IDZViewCell.h"
+
+@interface IDZViewCell ()
+
+@end
+
+@implementation IDZViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
