01b6e22675eed80e2f585ef661825d00e3c193bd
Anthony Powles <github@idreamz.net>
2014-01-20 22:19:00 -0800

    initial commit

---
+++ ToDoList/IDZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDZAppDelegate.h
+//  ToDoList
+//
+//  Created by Anthony Powles on 20/01/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Anthony Powles on 20/01/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDZAppDelegate class]));
+	}
+}
