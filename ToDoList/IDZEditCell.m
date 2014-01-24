85a84d684efdedcb2b40f5e662ea876885e14d33
Anthony Powles <github@idreamz.net>
2014-01-23 18:53:30 -0800

    WIP checkbox

--- ToDoList/IDZEditCell.m
+++ ToDoList/IDZEditCell.m
@@ -19,11 +19,29 @@
     return self;
 }
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+	}
+	return self;
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)viewDidLoad
+{
+	[self.todoCheckbox addTarget:self action:@selector(checkboxDidChanged:) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void)checkboxDidChanged:(CTCheckbox *)checkbox
+{
+    NSLog(@"%d", checkbox.checked);
 }
 
 @end
